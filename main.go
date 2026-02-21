c5203c98c034325e66b3232ccadfef73d6927a97
Mehmet Akif Ozdemir <akifz2199@gmail.com>
2026-02-21 22:55:49 +0300

    feat: add main entry point and update module deps with clipboard support

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/akif/go-brew/internal/ui"
+	tea "github.com/charmbracelet/bubbletea"
+)
+
+func main() {
+	m := ui.New()
+	p := tea.NewProgram(m, tea.WithAltScreen())
+	if _, err := p.Run(); err != nil {
+		fmt.Fprintf(os.Stderr, "go-brew: %v\n", err)
+		os.Exit(1)
+	}
+}
